755e4d813d8631ffae8c728545dd9fc0dd7fb935
Sahil Maheshwari <sahilm1711@gmail.com>
2022-08-30 12:35:48 +0530

    changing image links

--- components/SectionCard.jsx
+++ components/SectionCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SectionCard() {
   return (
     <div className="flex flex-col bg-white shadow m-2 pb-5 rounded-md">
-      <img src="./image16.png" className="max-w-[300px] p-0.5  m-auto"/>
+      <img  src="./image16.png" className="max-w-[300px] p-0.5  m-auto"/>
       <div className="text-black m-auto text-center">
         <h3 className="font-semibold mb-5 mt-5">Learn Everything about managing and operating a Self Storage</h3>
         <div className="flex items-center justify-center pb-1">
--- components/Shipping.jsx
+++ components/Shipping.jsx
@@ -3,7 +3,7 @@ import SectionCard from './SectionCard'
 function Shipping() {
   return (
     <>
-    <div className="bg-[url('../public/shippingBg.svg')] bg-cover rounded-br-[250px] flex justify-around items-center ">
+    <div className="bg-[url('/shippingBg.svg')] bg-cover rounded-br-[250px] flex justify-around items-center ">
         <div className='flex flex-col flex-start ml-20'>
             <h1 className='text-4xl font-bold bg-black-700 font-bold mb-5'>Do you have a Self Storage for Sale?</h1>
             <p className='text-xl text-[#000103]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tortor sapien sagittis maecenas tincidunt. Quis pretium ornare id diam amet proin scelerisque nunc.</p>
@@ -19,7 +19,7 @@ function Shipping() {
             <button className='text-xl text-white bg-blue-700 px-2 py-2 rounded-md w-2/6 mt-5'>Sell Your Self Storage</button>
         </div>
     </div>
-    <div className="bg-[url('../public/Frame12.png')] bg-cover text-center text-white pb-36  -mt-52 rounded-tl-[250px]">
+    <div className="bg-[url('/Frame12.png')] bg-cover text-center text-white pb-36  -mt-52 rounded-tl-[250px]">
          <h1 className='font-semibold text-4xl p-10 w-1/2 m-auto'>Learn Everything About Buying, Selling, and Operating a Self Storage</h1>
          <h3 className='text-xl pt-5'>Learn about literally anything to know about Selling or Buying a Self Storage</h3>
          <div className='flex w-4/5 mx-auto my-10'>
